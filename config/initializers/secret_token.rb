d8d608a226c0bcd724cf47b757904723cb5caa17
dklounge <daviddarden11@gmail.com>
2013-09-06 14:05:23 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Angelskills::Application.config.secret_key_base = '7514aeafc3d3e2cfbc322c2de1bfee445017fa7fa681222456cb131700f0f6861245645b8d448ce21c81457c7ddddb8f89e439a6da5ba5ce67384d0bee4d67a4'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Angelskills::Application.config.session_store :cookie_store, key: '_angelskills_session'
