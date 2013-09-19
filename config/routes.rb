2780d69cd118fd3a6343b5916c3b4ae1bb729af7
dklounge <daviddarden11@gmail.com>
2013-09-19 00:51:51 -0700

    added sessions controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 Angelskills::Application.routes.draw do
 
+  get 'login', :to => "sessions/new", :as => :login
+  get '/auth/:provider/callback', :to => "sessions#create"
+  get '/auth/failure', :to => "sessions#failure"
+
   root :to => 'skills#index'
   resources :skills
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
