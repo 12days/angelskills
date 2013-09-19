1abd72bff56bc033232d73b2fbec15de632f4938
dklounge <daviddarden11@gmail.com>
2013-09-19 00:45:12 -0700

    updated omniauth scope and fields

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,3 +1,6 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :linkedin, "aglkd3a476p2", "bRsZRj2XrtXy6GNE"
+  provider :linkedin, "aglkd3a476p2", "bRsZRj2XrtXy6GNE",
+  :scope => 'r_fullprofile r_emailaddress r_network',
+  :fields => ["id", "email-address", "first-name", "last-name", "headline",
+    "industry", "picture-url", "public-profile-url", "location", "connections"]
 end
