03ff5f198f52978247ae882a18452a78bd85d267
dklounge <daviddarden11@gmail.com>
2013-09-24 11:24:44 -0700

    sessions updates

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,11 @@
 class User < ActiveRecord::Base
   has_many :authorizations
   validates :name, :email, :presence => true
+
+  def add_provider(auth_hash)
+    # check if the provider already exists to avoid duplication
+    unless authorizations.find_by_provider_and_uid(auth_hash['provider'], auth_hash['uid'])
+      Authorization.create :user => self, :provider => auth_hash['provider'], :uid => auth_hash['uid']
+    end
+  end
 end
