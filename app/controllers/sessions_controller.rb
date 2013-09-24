533058c103e0b561def561e3ec6f6f76dbdd8a84
dklounge <daviddarden11@gmail.com>
2013-09-24 15:07:02 -0700

    fixed conflict in sessions controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -11,19 +11,12 @@ class SessionsController < ApplicationController
 
       render :text => "You can now login using #{auth_hash["provider"].capitalize} too!"
     else
-<<<<<<< HEAD
-      # user = User.new :name => auth_hash["user_info"]["name"], :email => auth_hash["user_info"]['email']
-      # user.authorizations.build :provider => auth_hash["provider"], :uid => auth_hash['uid']
-      # user.save
-      render :text => 'Success! Welcome #{user.name}!'
-=======
       # Log him in or sign him up
       auth = Authorization.find_or_create(auth_hash)
 
       # Create the session
       session[:user_id] = auth.user
       render :text => "Welcome #{auth.user.name}!"
->>>>>>> sessions
     end
   end
 
