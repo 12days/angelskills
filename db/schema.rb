d878322290f68c5f7090fa34989eaa912e110975
dklounge <daviddarden11@gmail.com>
2013-09-27 14:42:51 -0700

    added name and description columns to Skills table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20130924173506) do
+ActiveRecord::Schema.define(version: 20130927192809) do
 
   create_table "authentications", force: true do |t|
     t.integer  "user_id"
@@ -32,6 +32,8 @@ ActiveRecord::Schema.define(version: 20130924173506) do
   create_table "skills", force: true do |t|
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "name"
+    t.string   "description"
   end
 
   create_table "users", force: true do |t|
