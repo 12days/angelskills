e74678b84417169647165b6d797a4cbd69f8a442
dklounge <daviddarden11@gmail.com>
2013-09-27 12:19:07 -0700

    added skills views and form

--- app/controllers/skills_controller.rb
+++ app/controllers/skills_controller.rb
@@ -1,9 +1,17 @@
 class SkillsController < ApplicationController
 
+  def new
+    @skill = Skill.new
+  end
+
   def index
   end
 
   def create
+    @skill = Skill.new
+    @skill.title = params[:skill][:title]
+    @skill.save
+    redirect_to skill_path
   end
 
   def show
