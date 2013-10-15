823fbaf7caa2d3bef8afeb96b7aeefcdf593adaf
dklounge <daviddarden11@gmail.com>
2013-10-14 18:25:44 -0700

    fixed mistake in route

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Angelskills::Application.routes.draw do
   root :to => 'skills#index'
   resources :skills
 
-  devise_for :users, :controllers => { :omniauth_callbacks => 'users/omniauth_callbacks' }
+  devise_for :users, :controllers => { :omniauth_callbacks => 'omniauth_callbacks' }
                         # , path_names: { sign_in: 'login', sign_out: 'logout' }
   devise_scope :user do
     get 'login', :to => "devise/sessions#new"
