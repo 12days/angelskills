e8334ea2fccb4915c77c531f5f4389c74e7eceea
dklounge <daviddarden11@gmail.com>
2013-10-14 21:17:08 -0700

    made auth fixes

--- db/schema.rb
+++ db/schema.rb
@@ -13,14 +13,6 @@
 
 ActiveRecord::Schema.define(version: 20130927192809) do
 
-  create_table "authentications", force: true do |t|
-    t.integer  "user_id"
-    t.string   "provider"
-    t.string   "uid"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
-
   create_table "authorizations", force: true do |t|
     t.string   "provider"
     t.string   "uid"
