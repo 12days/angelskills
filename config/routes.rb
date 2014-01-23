6e3535177ed2b81c3c35b75baaf80c3f9b3c777d
David Kim <daviddarden11@gmail.com>
2014-01-22 19:22:27 -0800

    app yml

--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,9 @@ Angelskills::Application.routes.draw do
   root :to => 'skills#index'
   resources :skills
 
-  get '/auth/:provider/callback', :to => "sessions#create"
+  get '/auth/:provider/callback', :to => "sessions#create", :as => :linkedin
   get '/auth/failure', :to => "sessions#failure"
-  # get '/login' => "sessions#new", :as => :login
+  get '/login' => "sessions#new", :as => :login
   get "/logout" => "sessions#destroy", :as => :logout
 
 end
